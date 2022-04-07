9e96204eb9a964a848c1bcc24c35c141adae66f8
John Kirch <dubskigaming@hotmail.com>
2022-04-07 10:47:15 -0400

    changes with react 18

--- testing/src/components/__tests__/App.test.js
+++ testing/src/components/__tests__/App.test.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import { render, unmountComponentAtNode } from 'react-dom';
 import App from '../App';
 
 it('shows a comment box', () => {
   const div = document.createElement('div');
 
-  ReactDOM.render(<App />, div);
+  render(<App />, div);
 
-  console.log(div.innerHTML);
+  expect(div.innerHTML).toContain('Comment Box');
 
-  ReactDOM.unmountComponentAtNode(div);
+  unmountComponentAtNode(div);
 });
